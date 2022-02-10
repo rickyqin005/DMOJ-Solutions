1068823e137fc137cdb9cc60e269c1b4a410261e
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2022-02-10 13:56:55 -0500

    Delete main directory

--- main/CCC '13 J2 - Rotating letters 3.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-public class Question3 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine().replaceAll("[IOSHZXN]","");
-        if(str.length() == 0) System.out.println("YES");
-        else System.out.println("NO");
-    }
-}
